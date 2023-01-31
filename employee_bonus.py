c769a6e97139891991aad8c9c6eefa9418de4470
Lainey Urbas <lainey_urbas1@baylor.edu>
2023-01-30 23:22:48 -0600

    fixed 2

--- employee_bonus.py
+++ employee_bonus.py
@@ -2,6 +2,14 @@ import csv
 
 infile = open("EmployeePay.csv", "r")
 file2 = csv.reader(infile, delimiter=",")
-for a in file2:
-    print(a[1], a[2], a[3], a[4])
-infile.close
+next(file2)
+for item in file2:
+    print("First Name is", item[1])
+    print("Last Name is", item[2])
+    print("Salary amount $" + item[3])
+    bonusamt = int(item[3]) * float(item[4])
+    total = int(item[3]) + (bonusamt, 2)
+    print("Bonus amount $" + str(bonusamt, 2))
+    print("Total amount $" + str(total))
+    input()
+infile.close()
